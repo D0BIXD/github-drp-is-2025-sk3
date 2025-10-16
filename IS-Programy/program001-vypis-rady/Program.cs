c40998927e023351a2520b44b66f9d81b329f3a9
Daniel Rocha Pinheiro <daniel.rocha.pinheiro@tul.cz>
2025-10-16 16:29:11 +0200

    #3 Výpis řady vstup

--- IS-Programy/program001-vypis-rady/Program.cs
+++ IS-Programy/program001-vypis-rady/Program.cs
@@ -22,6 +22,23 @@ while (again == "a")
         Console.Write("Nezadali jste celé číslo. Zadejte první číslo řady znovu: ");
     }
 
+
+    Console.Write("Zadejte poslední číslo řady (celé číslo): ");
+    int last;
+    while (!int.TryParse(Console.ReadLine(), out last))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte poslední číslo řady znovu: ");
+    }
+
+
+    Console.Write("Zadejte diferenci (celé číslo): ");
+    int step;
+    while (!int.TryParse(Console.ReadLine(), out step))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte difereci znovu: ");
+    }
+
+
     Console.WriteLine();
     Console.WriteLine("Pro opakování programu stiskněte klávesu a");
     again = Console.ReadLine();
