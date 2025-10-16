805e833c0d3128f454723c8585909e71b5b64732
Daniel Rocha Pinheiro <daniel.rocha.pinheiro@tul.cz>
2025-10-16 16:20:34 +0200

    #4 zakladní kod ze školy tentokrát

---
+++ IS-Programy/program000a-zakladni-kod/Program.cs
@@ -0,0 +1,32 @@
+﻿string again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("************************************************");
+    Console.WriteLine("************** Výpis číselne řady **************");
+    Console.WriteLine("************************************************");
+    Console.WriteLine("************* Daniel Rocha Pinheiro ************");
+    Console.WriteLine("****************** 16.10.2025 ******************");
+    Console.WriteLine("************************************************");
+    Console.WriteLine();
+
+    // Vstup číselné hodnoty do programu - špatné řešení
+    // Console.Write("Zadejte první číslo řady: ");
+    // int first = int.Parse(Console.ReadLine());
+
+    // Vstup číselné hodnoty do programu - lepší řešení
+    Console.Write("Zadejte první číslo řady (celé číslo): ");
+    int first;
+    while (!int.TryParse(Console.ReadLine(), out first))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte první číslo řady znovu: ");
+    }
+
+    Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+    again = Console.ReadLine();
+    
+
+
+
+}
