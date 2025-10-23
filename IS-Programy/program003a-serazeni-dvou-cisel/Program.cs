bc45f482b72a10a369235bb03efec4e228ead10d
Daniel Rocha Pinheiro <daniel.rocha.pinheiro@tul.cz>
2025-10-23 16:28:00 +0200

    #5 Razeni dvou cisel

---
+++ IS-Programy/program003a-serazeni-dvou-cisel/Program.cs
@@ -0,0 +1,50 @@
+﻿string again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("************************************************");
+    Console.WriteLine("************** Výpis číselne řady **************");
+    Console.WriteLine("************************************************");
+    Console.WriteLine("************* Daniel Rocha Pinheiro ************");
+    Console.WriteLine("****************** 16.10.2025 ******************");
+    Console.WriteLine("************************************************");
+    Console.WriteLine();
+
+    // Vstup číselné hodnoty do programu - špatné řešení
+    // Console.Write("Zadejte první číslo řady: ");
+    // int first = int.Parse(Console.ReadLine());
+
+    // Vstup číselné hodnoty do programu - lepší řešení
+    Console.Write("Zadejte první číslo řady (celé číslo): ");
+    int first;
+    while (!int.TryParse(Console.ReadLine(), out first))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte první číslo řady znovu: ");
+    }
+
+
+
+    Console.Write("Zadejte poslední číslo řady (celé číslo): ");
+    int last;
+    while (!int.TryParse(Console.ReadLine(), out last))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte poslední číslo řady znovu: ");
+    }
+
+
+    Console.Write("Zadejte diferenci (celé číslo): ");
+    int step;
+    while (!int.TryParse(Console.ReadLine(), out step))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte difereci znovu: ");
+    }
+
+
+    Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+    again = Console.ReadLine();
+    
+
+
+
+}
